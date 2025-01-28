98774f21b69d6f307206f71aeed03fcedc11c573
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2025-01-27 21:08:30 -0800

    testing with error_c
    Signed-off-by: Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -113,6 +113,6 @@
 /* Error messages and TLS debugging traces
  * (huge code size increase, needed for tests/ssl-opt.sh) */
 //#define MBEDTLS_DEBUG_C
-//#define MBEDTLS_ERROR_C
+#define MBEDTLS_ERROR_C
 
 #endif /* MBEDTLS_CONFIG_H */
